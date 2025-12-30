1798ad711232279ba073549408d1b0400f771674
Sheldon Wang <wangshenglin@sjtu.edu.cn>
2025-12-30 12:18:32 +0800

    generate ITER_cocos02.json

--- geometry/process_qedsk.py
+++ geometry/process_qedsk.py
@@ -1,3 +1,4 @@
+import json
 import re
 import matplotlib.pyplot as plt
 
@@ -16,9 +17,26 @@ with open('iterhybrid_cocos02.eqdsk', 'r') as f:
         p.extend([float(x) for x in found_numbers])
 
 plt.figure(figsize=(4, 6))
+zs, rs = [], []
 for i in range(0, len(p) - 1, 2):
-    x, y = p[i], p[i + 1]
-    plt.scatter(x, y, c='g', s=2)
+    r, z = p[i], p[i + 1]
+    if abs(z) > 3.99:
+        print(i)
+        plt.scatter(r, z, c='r', s=2)
+    else:
+        rs.append(r)
+        zs.append(z)
+        plt.scatter(r, z, c='g', s=2)
+
 plt.grid()
 plt.show()
 print(data)
+
+js = json.load(open('../PAM/template_D3D_1layer_2species.json'))
+
+# update (Z, R)
+js['equilibrium']['time_slice'][0]['boundary']['outline']['z'] = zs
+js['equilibrium']['time_slice'][0]['boundary']['outline']['r'] = rs
+
+
+json.dump(js, open('../PAM/ITER_cocos02.json', 'w'))
